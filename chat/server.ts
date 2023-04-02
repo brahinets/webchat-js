de6f4caff4113e20d199fb85a7fb0b8e1ae98d39
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-02 17:54:32 +0300

    Broadcast messages via SSE

--- chat/server.ts
+++ chat/server.ts
@@ -1,8 +1,40 @@
-import express from 'express';
+import express from 'express'
+import bodyParser from 'body-parser'
+import {v4 as uuidv4} from 'uuid';
 
 const app: express = express()
-
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static('chat/public'))
+
+const clientsMap: Map<string, express.Response> = new Map<string, express.Response>();
+
+app.get('/register', (req: express.Request, res: express.Response): void => {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    const clientID = uuidv4();
+    clientsMap.set(clientID, res);
+
+    req.on('close', (): void => {
+        clientsMap.delete(clientID);
+    });
+});
+
+app.post('/message', (req, res): void => {
+    const message: string = req.body;
+
+    clientsMap.forEach((clientConnection: express.Response, clientID: string): void => {
+        console.log(`Broadcasting message to ${clientID}`)
+        clientConnection.write(`data: ${message}\n\n`)
+    });
+
+    res.sendStatus(200);
+});
 
 const port: number = 3000;
 app.listen(port, (): void => {
