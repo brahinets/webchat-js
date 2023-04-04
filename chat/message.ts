f01a7a64235ecefe132b872608d3c2ae7416f7fc
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-04 22:36:44 +0300

    Use separate model for transport

--- chat/message.ts
+++ chat/message.ts
@@ -1,9 +1,21 @@
 export class Message {
     readonly authorID: string;
-    readonly message: string;
+    readonly text: string;
 
     constructor(authorID: string, message: string) {
         this.authorID = authorID;
-        this.message = message;
+        this.text = message;
+    }
+}
+
+export class MessageDto {
+    readonly authorID: string;
+    readonly authorName: string;
+    readonly text: string;
+
+    constructor(authorID: string, authorName: string, message: string) {
+        this.authorID = authorID;
+        this.authorName = authorName;
+        this.text = message;
     }
 }
--- chat/server.ts
+++ chat/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import {ChatService, Participant} from "./chat-service";
-import {Message} from "./message";
+import {Message, MessageDto} from "./message";
 
 
 const app: express = express()
@@ -30,26 +30,21 @@ app.get('/register', (req: express.Request, res: express.Response): void => {
 
     const id: string = req.query.clientID;
     const participant: Participant = chatService.userById(id);
-    if(participant === undefined) {
+    if (participant === undefined) {
         return;
     }
 
     clientsMap.set(participant, res);
 
-    chatService.getAllMessages().forEach((message:Message) => {
-        res.write('data: ' + JSON.stringify({
-            authorID: message.authorID,
-            authorName: chatService.userById(message.authorID).name,
-            message: message.message
-        }) + '\n\n');
+    chatService.getAllMessages().forEach((message: Message) => {
+        res.write('data: ' + JSON.stringify(toMessageDto(message)) + '\n\n');
     });
 
     clientsMap.forEach((clientConnection: express.Response, targetParticipant: Participant): void => {
         console.log(`Broadcasting message to ${targetParticipant.id}`)
-        clientConnection.write('data: ' + JSON.stringify({
-            authorName: participant.name,
-            message: "has joined chat"
-        }) + '\n\n');
+        clientConnection.write('data: ' + JSON.stringify(toMessageDto(
+            new Message(participant.id, "has joined char"))
+        ) + '\n\n');
     });
 
     req.on('close', (): void => {
@@ -60,22 +55,21 @@ app.get('/register', (req: express.Request, res: express.Response): void => {
 app.post('/message', (req, res): void => {
     const message = JSON.parse(req.body);
 
-    let messageData:Message = new Message(message.clientID, message.message);
+    const messageData: Message = new Message(message.clientID, message.message);
     chatService.addMessage(messageData)
 
     clientsMap.forEach((clientConnection: express.Response, participant: Participant): void => {
         console.log(`Broadcasting message to ${participant.id}`)
-        const author: Participant = chatService.userById(messageData.authorID);
-
-        clientConnection.write('data: ' + JSON.stringify({
-            authorID: author.id,
-            authorName: author.name,
-            message: messageData.message
-        }) + '\n\n');
+        clientConnection.write('data: ' + JSON.stringify(toMessageDto(messageData)) + '\n\n');
     });
 
     res.sendStatus(200);
 });
+
+function toMessageDto(message: Message): MessageDto {
+    const author: Participant = chatService.userById(message.authorID);
+    return new MessageDto(author.id, author.name, message.text);
+}
 
 const port: number = 3000;
 
